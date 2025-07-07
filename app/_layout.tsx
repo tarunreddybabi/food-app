5a8efe6e80222249f2ace17df38ee32f752d6d2a
tarun <tarunreddy.b@enterpi.com>
2025-07-07 11:34:27 +0530

    added fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,22 @@
+import { SplashScreen, Stack } from "expo-router";
+import {useFonts} from 'expo-font'
+import { useEffect } from "react";
+
 import "./global.css"
-import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded,error]=useFonts({
+    "QuickSand-Bold":require('../assets/fonts/Quicksand-Bold.ttf'),
+    "QuickSand-Medium":require('../assets/fonts/Quicksand-Medium.ttf'),
+    "QuickSand-Regular":require('../assets/fonts/Quicksand-Regular.ttf'),
+    "QuickSand-SemiBold":require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    "QuickSand-Light":require('../assets/fonts/Quicksand-Light.ttf')
+  })
+
+  useEffect(()=>{
+    if(error) throw Error;
+    if(fontsLoaded) SplashScreen.hideAsync()
+  },[fontsLoaded,error]);
+
+  return <Stack screenOptions={{headerShown:false}}/>;
 }
